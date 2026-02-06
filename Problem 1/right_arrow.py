1d47d6312d3fded804c3308ed044a3fe8c4c1e3c
Stephen Cecka <scecka04@gmail.com>
2026-02-06 16:21:06 -0600

    lab 2 problem 2 complete

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -3,7 +3,11 @@ def right_arrow():
     head_char = input()
 
     row1 = '      ' + head_char
-    ''' Type your code here. '''
+    row2 = base_char *6 + head_char *2
+    row3 = base_char *6 + head_char *3
+    row4 = base_char *6 + head_char *2
+    row5 = '      ' + head_char
+    
 
     print(row1)
     print(row2)
--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,5 +1,13 @@
 def telephone():
     phone_number = int(input())
+    
+    area_code = phone_number // 10000000
+    prefix = phone_number % 10000000 // 10000
+    line_number = phone_number % 10000
+
+    print(f"({area_code}) {prefix}-{line_number}")
+
+
     ''' Type your code here. '''
 if __name__ == "__main__":
     telephone()
