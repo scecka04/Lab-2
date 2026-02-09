741b3d334b38ce7223b64ca20dd7e870ac670b7c
Stephen Cecka <scecka04@gmail.com>
2026-02-09 13:10:25 -0600

    lab 2 problem 3 complete

--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -1,6 +1,14 @@
 
 def caffeine():
     caffeine_mg = float(input())
+    
+    after_6 = caffeine_mg * .50
+    after_12 = caffeine_mg * .50 ** 2
+    after_24 = caffeine_mg * .50 ** 4
+
+    print(f'After 6 hours: {after_6:.2f} mg')
+    print(f'After 12 hours: {after_12:.2f} mg')
+    print(f'After 24 hours: {after_24:.2f} mg')
     ''' Type your code here. '''
     
 if __name__ == "__main__":
