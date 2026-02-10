887175c32a6d154793c99a9098f4165c5cc49227
Stephen Cecka <scecka04@gmail.com>
2026-02-09 22:37:24 -0600

    lab 2 all problems compete

--- Problem 5/simple_stats.py
+++ Problem 5/simple_stats.py
@@ -4,6 +4,13 @@ def simple_stats():
     num2 = float(input())
     num3 = float(input())
     num4 = float(input())
+
+    product = num1 * num2 * num3 * num4
+    average = (num1 + num2 + num3 + num4) / 4
+
+    print(f'{product:.0f} {average:.0f}')
+    print(f'{product:.3f} {average:.3f}')
+
     ''' Type your code here. '''
 if __name__ == "__main__":
     simple_stats()
